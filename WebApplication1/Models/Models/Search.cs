c762a2efdd1015b1eb309cd7fa66e95f7496898e
unknown <mochidashi@DESKTOP-MG64FI0.local.ffsp.co.jp>
2020-09-10 10:19:53 +0900

    com

---
+++ WebApplication1/Models/Models/Search.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class Search
+    {
+    }
+
+    public class Flashcard
+    {
+        public int ID { get; set; }
+        public string Word { get; set; }
+        public string Meaning { get; set; }
+        public string Remarks { get; set; }
+    }
+
+
+    public class Sqlsvr : DbContext
+    {
+        public DbSet<Flashcard> Flashcards { get; set; }
+    }
+}
