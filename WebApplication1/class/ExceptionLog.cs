13e0fa9c77c2e7bd23030230c246e2e12cedaf0e
unknown <mochidashi@DESKTOP-MG64FI0.local.ffsp.co.jp>
2020-09-10 14:29:05 +0900

    com

--- WebApplication1/class/ExceptionLog.cs
+++ WebApplication1/class/ExceptionLog.cs
@@ -13,19 +13,19 @@ namespace WebApplication1
     {
         public void writeLod(string log)
         {
-            //string filePass = @"C:\Users\h2-ota\source\repos\WebApplication1\Log.txt";
-            var filePass = System.Configuration.ConfigurationManager.AppSettings["LogFilePath"].ToString();
-            if(File.Exists(filePass) == false)
-            {
-                filePass = @"C:\Users\h2-ota\source\repos\WebApplication1\Log.txt";
-            }
+            ////string filePass = @"C:\Users\h2-ota\source\repos\WebApplication1\Log.txt";
+            //var filePass = System.Configuration.ConfigurationManager.AppSettings["LogFilePath"].ToString();
+            //if(File.Exists(filePass) == false)
+            //{
+            //    filePass = @"C:\Users\h2-ota\source\repos\WebApplication1\Log.txt";
+            //}
 
-            using (FileStream fs = new FileStream(filePass, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read))
-            using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
-            {
-                string fmt = @"{0} {1}";
-                sw.WriteLine(string.Format(fmt, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), log));
-            }
+            //using (FileStream fs = new FileStream(filePass, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read))
+            //using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+            //{
+            //    string fmt = @"{0} {1}";
+            //    sw.WriteLine(string.Format(fmt, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), log));
+            //}
         }
     }
 }
